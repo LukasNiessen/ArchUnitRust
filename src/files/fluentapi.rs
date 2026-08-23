e0fb19bbe70204637cd3c3014ca74d5490ed86d6
Niessen <Lukas.Niessen@bain.com>
2026-08-23 19:48:03 +0200

    feat: add positive and negated file moods

--- src/files/fluentapi.rs
+++ src/files/fluentapi.rs
@@ -2,6 +2,12 @@
 
 mod file_condition_builder;
 mod files;
+mod match_pattern_file_condition_builder;
+mod negated_match_pattern_file_condition_builder;
+mod positive_match_pattern_file_condition_builder;
 
 pub use file_condition_builder::FileConditionBuilder;
 pub use files::{files, files_in, project_files, project_files_in};
+pub use match_pattern_file_condition_builder::MatchPatternFileConditionBuilder;
+pub use negated_match_pattern_file_condition_builder::NegatedMatchPatternFileConditionBuilder;
+pub use positive_match_pattern_file_condition_builder::PositiveMatchPatternFileConditionBuilder;
